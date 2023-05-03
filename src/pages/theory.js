dc773653b710f02af2f0a4e67219f059b08b3a20
Rayson Kong <kong.rayson@gmail.com>
2023-05-03 19:34:34 +0800

    setup theory page

--- src/pages/theory.js
+++ src/pages/theory.js
@@ -8,9 +8,56 @@ import Seo from "../components/seo"
 
 const Theory = () => (
   <Layout>
-      <h1>Music Theory </h1>
+      <h1>Music Theory Course Videos</h1>
 
-      <Link to="/">Home</Link> <br />
+      <table class="GeneratedTable"  class="last">
+        <thead>
+          <tr>
+            <th>Resources</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr>
+            <td><a href="" target="_blank"> </a></td>
+            <td><a href="" target="_blank"></a></td>
+          </tr>
+
+          <tr>
+            <td><a href="" target="_blank"></a></td>
+            <td><a href="" target="_blank"></a></td>
+          </tr>
+
+        </tbody>
+      </table>
+      
+        <table class="GeneratedTable"  class="last">
+          <thead>
+            <tr>
+              <th>Ch 1 - Reading Music</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <tr>
+              <td><a href="https://youtu.be/kvDQHnQG-9Y" target="_blank">Ch1 Part 1: Reading Music 1 </a></td>
+              <td><a href="https://sowl.co/s/UeTgR" target="_blank"></a></td>
+            </tr>
+
+
+            <tr>
+              <td><a href="https://youtu.be/HHNPZl0gY0g" target="_blank">Ch1 Part 2: Reading Music 2</a></td>
+              <td><a href="https://sowl.co/s/hUBwZ" target="_blank"></a></td>
+            </tr>
+
+          </tbody>
+        </table>
+
+
+
+        
 
   </Layout>
 )
