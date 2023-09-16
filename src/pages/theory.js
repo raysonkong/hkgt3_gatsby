de571c96f00fc5860640c9b546f3817897db4c38
Rayson Kong <kong.rayson@gmail.com>
2023-09-16 21:15:44 +0800

    ch2

--- src/pages/theory.js
+++ src/pages/theory.js
@@ -100,8 +100,6 @@ const Theory = () => (
              <td><a href="https://youtu.be/-wHL7fDQaw8" target="_blank">Ch1 Reading Music Part 14: Reading Leger Lines (Bass Low)</a></td>
            </tr>
 
-
-
           </tbody>
         </table>
 
@@ -115,12 +113,117 @@ const Theory = () => (
           <tbody>
 
             <tr>
-              <td><a href="https://youtu.be/kvDQHnQG-9Y" target="_blank">Ch1 Part 1: Reading Music 1 </a></td>
+              <td><a href="https://youtu.be/CupiUVkITKU?si=TH-hSXnGwIZkpNA1" target="_blank">Ch 2 Intervals Part 1: Why intervals is important
+ </a></td>
             </tr>
 
 
             <tr>
-              <td><a href="https://youtu.be/HHNPZl0gY0g" target="_blank">Ch1 Part 2: Reading Music 2</a></td>
+              <td><a href="https://youtu.be/LxX9j0UO_bc?si=Iq6GSADMXMLi9qo0" target="_blank">Ch 2 Intervals Part 2: Intervals Quantity and Counting</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/OY5EW9jgPZY?si=5Iq74kW8lKXU-PeZ" target="_blank">Ch 2 Intervals Part 3: Interval Quality and 2nds
+</a></td>
+            </tr>
+
+
+            <tr>
+              <td><a href="https://youtu.be/-hY2IbDPjXg?si=lJuHfHRGVBuVAHH6" target="_blank">Ch 2 Intervals Part 4: Interval Exercise (2nds)
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/jtZ0jlSZtD8?si=ms35-dfVDgd_s1YO" target="_blank">Ch 2 Intervals Part 5: Intervals Aug and Dim (2nds)
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/tg7D3MeLx3E?si=mYu5bAIQ2-j7KhRT" target="_blank">Ch 2 Intervals Part 6: Major and minor 3rds
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/itaigaWBo7Q?si=LJbJGWyX0659snmi" target="_blank">Ch 2 Intervals Part 7: Aug 3rd and Dim 3rds
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/OFPsRYjQUXM?si=zsOOr5MurpgNPEwT" target="_blank">Ch 2 Intervals Part 8: Perfect 4ths and Aug/Dim
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/awnGBLWp4Qw?si=4sDp7in68k31zEKZ" target="_blank">Ch 2 Intervals Part 9: Perfect 5ths
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/tHsoUJQ289E?si=Jl3mGNn6EUqwP1u7" target="_blank">Ch 2 Intervals Part 10:Aug/Dim 5ths
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/2q2bKu_WtSA?si=U0fl50qKuCpr3arD" target="_blank">Ch 2 Intervals Part 11: Major and Minor 6th
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/gzBpZBXtCOc?si=JOPMj5Bn2F2xPLLn" target="_blank">Ch 2 Intervals Part 12: Aug/Dim 6th
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/DpuwWIZL9GE?si=o7pfq2W_Us1fzyou" target="_blank">Ch 2 Intervals Part 13: Major and Minor 7ths
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/2IcTOC2A0e4?si=vfMxYsA_H5EAw9yt" target="_blank">Ch 2 Intervals Part 14: 7ths Exercises
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/sqQKcqZprrs?si=NgpiiWCHiJtyFwF6" target="_blank">Ch 2 Intervals Part 15: Test review part 1
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/5sjBYPHV3e8?si=9dBxKVjxYQfYXeVf" target="_blank">Ch 2 Intervals Part 16: Review part 2</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/5sjBYPHV3e8?si=9dBxKVjxYQfYXeVf" target="_blank">Ch 2 Intervals Part 16: Review part 2</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/MRBhhWgFTOY?si=A9iQThvyfFoXnk49" target="_blank">Ch 2 Intervals Part 17: Homework</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/QOB347DQeWo?si=O6-HlhvMh8a0uc35" target="_blank">Ch 2 Intervals Part 18: Exercise part 1</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/sN3u3XaUXuo?si=4F1M_mBXsfPJVZ1e" target="_blank">Ch 2 Intervals Part 19: Exercise Part 2</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/rk2l_77q5_g?si=ILET-EqRgGJmrQai" target="_blank">Ch 2 Intervals Part 20: Exercise Part 3A</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/HFopU_Ud6wc?si=XinCQ3CO9TQdBBMR" target="_blank">Ch 2 Intervals Part 21: Exercise Part 3B</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/7nTuPhME0eA?si=-YmASZtvaxAOTygW" target="_blank">Ch 2 Intervals Part 22: Exercise Part 4A
+</a></td>
+            </tr>
+
+            <tr>
+              <td><a href="https://youtu.be/pdIZR9D-sLk?si=zJX6Dl2UQnNv2fsv" target="_blank">Ch 2 Intervals Part 23: Exercise Part 4B
+</a></td>
             </tr>
 
           </tbody>
